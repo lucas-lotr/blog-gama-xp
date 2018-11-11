09abe245f9066b555007b58383b3dd82be5cfdf6
lucas-lotr <44841472+lucas-lotr@users.noreply.github.com>
2018-11-11 16:32:18 -0200

    firebase test

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,34 @@
-var submitButton=document.getElementById('submit-button')
+var submitButton=document.getElementById('submit-button');
+var nome=document.getElementById('input-nome');
+var database=firebase.database();
+
+function writeData(id,data){
+
+    database.ref('meta/').set({
+        qtd: id+1
+    });  
+
+     var updates = {};
+     updates['leads/' + id+1] = {
+         nome: data
+     };
+     
+
+    return firebase.database().ref().update(updates);
+
+
+
+}
 
 submitButton.onclick = function(){
-    console.log('ding!')
+    console.log(nome.value);
+
+    return firebase.database().ref('/meta/').once('value').then(function(snapshot) {
+        console.log(snapshot.val().qtd);
+
+        writeData(snapshot.val().qtd,nome.value);
+
+      });
+
+    
 }
